c423743e9bbd09c390117ca5131120a79445efaa
Sergio Andrés Bejarano Rodríguez <157920809+SergioBejarano@users.noreply.github.com>
2026-02-21 17:25:21 +0000

    test: add edge case tests

--- src/test/java/edu/university/tdd/FizzBuzzTest.java
+++ src/test/java/edu/university/tdd/FizzBuzzTest.java
@@ -23,4 +23,35 @@ class FizzBuzzTest {
     void testFizzBuzzMultiplo3y5() {
         assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(15));
     }
+
+        @Test
+        void testFizzBuzzZero() {
+            assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(0));
+        }
+
+        @Test
+        void testFizzBuzzNegativeThree() {
+            assertEquals("Fizz", FizzBuzz.fizzbuzz(-3));
+        }
+
+        @Test
+        void testFizzBuzzNegativeFive() {
+            assertEquals("Buzz", FizzBuzz.fizzbuzz(-5));
+        }
+
+        @Test
+        void testFizzBuzzNegativeFifteen() {
+            assertEquals("FizzBuzz", FizzBuzz.fizzbuzz(-15));
+        }
+
+        @Test
+        void testFizzBuzzLargeNumber() {
+            assertEquals("Fizz", FizzBuzz.fizzbuzz(99999));
+        }
+
+        @Test
+        void testFizzBuzzNotMultiple() {
+            assertEquals("2", FizzBuzz.fizzbuzz(2));
+            assertEquals("-2", FizzBuzz.fizzbuzz(-2));
+        }
 }
